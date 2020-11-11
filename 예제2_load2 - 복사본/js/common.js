fc556848ff3bf91e4f8d1b97720cc413aad1120c
myoungkyo <73566270+myoungkyo@users.noreply.github.com>
2020-11-11 12:01:36 +0900

    loadfile

---
+++ 예제2_load2 - 복사본/js/common.js
@@ -0,0 +1,26 @@
+// $(document).ready(function(){
+// });
+
+//약식
+$(function(){
+  /*script 영역 html load 사용 예*/
+  // $("ul li").click(function(){
+  //   var thisId = $(this).attr("id")
+  //   if(thisId == "section1"){
+  //     $("#content").load("./load_file.html #section1");
+  //   }else if(thisId == "section2"){
+  //     $("#content").load("./load_file.html #section2");
+  //   }else if(thisId == "section3"){
+  //     $("#content").load("./load_file.html #section3");
+  //   }
+  // });
+
+  $("ul li").click(function(){
+    //console.log($(this).index());
+    var thisIndex = $(this).index() + 1; //클릭하는요소의 인덱스 값을 받아온다(index는 0부터 시작하므로 +1)
+    var thisId = $(this).attr("id")
+    if(thisId == "section"+thisIndex){
+      $("#content").load("./load_file.html #section"+thisIndex);
+    }
+  });
+});
